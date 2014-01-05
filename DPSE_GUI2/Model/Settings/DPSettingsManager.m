5f3bc9c3d41658a5756f0bd8c307dd07e3d6b735
Petro Korienev <soxjke@gmail.com>
2014-01-05 04:15:56 +0200

    Added settings manager

---
+++ DPSE_GUI2/Model/Settings/DPSettingsManager.m
@@ -0,0 +1,60 @@
+//
+//  DPSettingsManager.m
+//  Distributed parallel simulation environment graphical user interface
+//
+//  Created by Petro Korienev on 1/5/14.
+
+//    Copyright (c) 2014 Petro Korienev. All rights reserved. 
+
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//  
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//  
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+
+
+#import "DPSettingsManager.h"
+
+@interface DPSettingsManager ()
+{
+    NSUserDefaults *defaults;
+}
+
+@end
+
+@implementation DPSettingsManager
+
++ (instancetype)sharedObject
+{
+    static id _sharedObject = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^(void)
+    {
+        _sharedObject = [[self alloc] init];
+    });
+    return _sharedObject;
+}
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self)
+    {
+        defaults = [NSUserDefaults standardUserDefaults];
+    }
+    return self;
+}
+
+@end
